1be319f31054a7a05bf37fda95ad8e3bf57faed6
Gabriel_Trajano <conversarcomgabriel@gmail.com>
2025-12-10 15:28:21 -0300

    Update: Comments on the script

--- script.py
+++ script.py
@@ -1,12 +1,23 @@
+#Importação da Biblioteca Requirida
 import qrcode
 
+# Variável onde será armazenada a URL:
 url = input("Enter the URL: ").strip()
-file_path = "//home//gabriel//Desktop//qrcode.png"
 
+# Caminho de Destino do QRCODE ( Ajuste conforme seu sistema operacional )
+file_path = "Seu caminho de destino no seu computador onde será salvo o seu QRCODE"
+
+# Inicialização do Gerador QR Code
 qr = qrcode.QRCode()
+
+# Adição dos Dados
 qr.add_data(url)
 
+# Geração da Imagem do QR Code
 img = qr.make_image()
+
+# Salvamento do Arquivo
 img.save(file_path)
 
+# Mensagem de Confirmação
 print("QR Code was generated!")
